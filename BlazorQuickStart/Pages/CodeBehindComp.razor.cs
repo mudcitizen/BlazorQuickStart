1016f5ea4bd526314dd70ff9b4fd35da4838a771
Kevin Lane <mudcitizen@gmail.com>
2019-11-22 20:54:08 -0500

    Added simple components

---
+++ BlazorQuickStart/Pages/CodeBehindComp.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorQuickStart.Pages
+{
+    public class CodeBehindCompModel : ComponentBase
+    {
+        public string PageTitle { get; set; } = "Component Demo / CodeBehind";
+    }
+}

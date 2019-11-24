74b82ccf2a8a367cae05e65f7b6cb599129ae260
Kevin Lane <mudcitizen@gmail.com>
2019-11-24 17:35:16 -0500

    Scaffolded stuff for JS Interop

---
+++ JSInteropDemo/Pages/CallJSMethod.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSInteropDemo.Pages
+{
+    public class CallJSMethodBase : ComponentBase
+    {
+        [Inject]
+        protected IJSRuntime _jsRuntime { get; set; }
+
+        protected string _message = "WTF?";
+        protected string message { 
+            get { 
+                return _message; 
+            }
+            set {
+                _message = value;
+            }
+        }
+
+      
+        protected void CallJSMethod() {
+            _jsRuntime.InvokeAsync<bool>("showAlertBox");
+        }
+        protected void CallJSMethodWithParameters()
+        {
+            _jsRuntime.InvokeAsync<bool>("showPrompt",message);
+        }
+    }
+}
